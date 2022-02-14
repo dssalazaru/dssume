cad9eafa77c19b9517de47822add3c0905ef3a29
dssalazaru <dssalazaru@gmail.com>
2022-02-14 18:07:29 -0500

    footer, nav and icons

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Variables
 
 const nav = document.querySelectorAll(".nav li")
-const content = document.querySelectorAll(".main-content")
+const content = document.querySelectorAll(".main-content section")
 
 // Click events | set var clickEvent with => click("#name")
 
@@ -18,22 +18,22 @@ nav.forEach(e => {
     e.addEventListener('click', e => {
         e.preventDefault()
         let opt = e.target.textContent
-        console.log(opt)
+        // console.log(opt)
         nav.forEach(e => {
             e.removeAttribute('class', 'active')
         });
         e.target.setAttribute('class', 'active')
-        
         loadData(opt)
     })
 })
 
 let loadData = (opt) => {
     content.forEach(e => {
-        e.setAttribute('class', 'hide')
+        e.classList.add('hide')
+        if (e.classList.contains(opt)){
+            e.classList.remove('hide')
+        }
     })
-    let active = '.' + opt
-    console.log(document.querySelector(active))
     // document.querySelector(active).removeAttribute('class', 'hide')
 }
 
@@ -46,7 +46,7 @@ const loadPage = () => {
         document.querySelector(".title").setAttribute("style", "border-right:0");
     }, 6000);
     setTimeout(function () {
-        document.querySelector(".name").textContent = "Daniel Salazar Steven Uribe";
+        document.querySelector(".name").textContent = "Daniel Steven Salazar Uribe";
     }, 6000);
     setTimeout(function () {
         document.querySelector("#screen").setAttribute("class", "lock transition-down");
