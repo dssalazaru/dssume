24ed0e9beb3fe97947fe75b986b2cd0f837c9edf
dssalazaru <dssalazaru@gmail.com>
2022-02-22 00:03:10 -0500

    Calc1 progress: 70%

--- testjs/calc1/main.js
+++ testjs/calc1/main.js
@@ -4,42 +4,84 @@ const td = document.querySelectorAll("td")
 
 td.forEach((e) => {
     e.addEventListener("click", (e) => {
-        isCero()
+        e.preventDefault()
         let inClass = e.target.getAttribute("class")
         let elem = e.target.textContent
-        if (inClass == "n") {
+        if (inClass == "display"){
+            e.preventDefault()
+        }
+        else if (inClass == "n") {
+            isCero()
             display.textContent += e.target.textContent
         }
-        if (inClass == "o") {
-            switch (elem) {
-                case "+":
-                    display.textContent += elem
-                    break
-                case "-":
-                    display.textContent += elem
-                    break
-                case "/":
-                    display.textContent += elem
-                    break
-                case "x":
-                    display.textContent += "*"
-                    break
-                default:
-                    console.error("xd")
+        else if (inClass == "o") {
+            if(display.textContent == 0){
+                e.preventDefault()
+            }else {
+                switch (elem) {
+                    case "+":
+                        display.textContent += elem
+                        break
+                    case "-":
+                        display.textContent += elem
+                        break
+                    case "/":
+                        display.textContent += elem
+                        break
+                    case "*":
+                        display.textContent += elem
+                        break
+                    default:
+                        console.error("xd")
+                }
             }
+            
         }
-        if (inClass == "ac") {
-            console.log(data.replace)
+        else if (inClass == "ac") {
+            let data = display.textContent
+            display.textContent = ""
+            for (let i = 0; i < (data.length -1); i++) {
+                display.textContent += data[i]
+            }
+        }else {
+            let dat = display.textContent
+            // let num = display.textContent.split(/[$*\-+/]/g)
+            // let ope = display.textContent.replace(/[$1-9]/g,'')
+            let num = ""
+            let ope = []
+            let n = [0,1,2,3,4,5,6,7,8,9]
+            let o = ['+', '-', '*', '/']
+            let con = 0
+            Array.from(dat).forEach((e) => {
+                if (e in n){
+                    num += e
+                    con = 0
+                } else if (o.includes(e)){
+                    // e.replace(/[$*\-\+/]+/g,e)
+                    if (num != '') {ope.push(num); num = ""}
+                    if (con == 0){
+                        ope.push(e)
+                        con++
+                    }
+                }
+            }) 
+            if (num != '') {ope.push(num); num = ""}
+            while (o.includes(ope[ope.length - 1]) || ope[ope.length - 1] == ''){
+                    ope.pop()
+            }
+            display.textContent = eval(ope.join(' '))
         }
     })
 })
 
-const sum = (n1, n2) => {
-    display.textContent = n1 + n2
+const lastElement = (lst) => {
+    lst 
 }
 
 
-
+const sum = (n1, n2) => {
+    display.textContent = n1 + n2
+}
 
 let isCero = () => {
     if (display.textContent == 0)
